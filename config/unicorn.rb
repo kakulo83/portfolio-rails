6c934af5b6fbc77968445e2d61615a115de464de
Robert Carter <robertcliffordcarter@gmail.com>
2026-01-16 13:14:43 -0800

    add cloudflare x-custom-header secret value, add unicorn logging

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,8 @@
+parent_dir = File.expand_path("..", __dir__)
+working_directory = parent_dir
+
+stderr_path "#{working_directory}/log/unicorn.stderr.log"
+stdout_path "#{working_directory}/log/unicorn.stdout.log"
+
 worker_processes 4
 listen "127.0.0.1:3000"
