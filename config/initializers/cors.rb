f48661b4ecfcf995df26eba94b8d84066f39b6f0
Robert Carter <robertcliffordcarter@gmail.com>
2026-01-19 15:26:01 -0800

    switch bcak to puma for simplicity

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5174"
+    origins "http://localhost:5174", "https://rcarter.io"
 
     resource "*",
       headers: :any,
